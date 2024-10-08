b0b6ca9422f1666c8e3c68defcbff880fab704a9
Jarry6304 <jarry6304@gmail.com>
2024-10-08 09:55:36 +0800

    SQL server connect

---
+++ prjNorthwindCustomer/DAO/CustomersDAO.cs
@@ -0,0 +1,11 @@
+﻿namespace prjNorthwindCustomer.DAO
+{
+    internal class CustomersDAO
+    {
+        private string _connectionString { get; set; }
+        internal CustomersDAO(string connection)
+        {
+            _connectionString = connection;
+        }
+    }
+}

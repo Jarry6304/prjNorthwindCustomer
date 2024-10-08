8cc83b836f4c9be90303d14828f0cb0f14838d69
Jarry6304 <jarry6304@gmail.com>
2024-10-08 10:38:02 +0800

    customerDAO

--- prjNorthwindCustomer/DAO/CustomersDAO.cs
+++ prjNorthwindCustomer/DAO/CustomersDAO.cs
@@ -1,4 +1,10 @@
-﻿namespace prjNorthwindCustomer.DAO
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using prjNorthwindCustomer.Models;
+using System;
+using static System.Collections.Specialized.BitVector32;
+
+namespace prjNorthwindCustomer.DAO
 {
     internal class CustomersDAO
     {
@@ -7,5 +13,32 @@
         {
             _connectionString = connection;
         }
+
+        internal void createNewCustomer(Customers data)
+        {
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = @"
+                    INSERT INTO Customers (CustomerID, CompanyName, ContactName, ContactTitle, Address, City, Region, PostalCode, Country, Phone, Fax)
+                    VALUES (@CustomerID, @CompanyName, @ContactName, @ContactTitle, @Address, @City, @Region, @PostalCode, @Country, @Phone, @Fax)";
+                connection.Execute(sqlQuery, new
+                {
+                    CustomerID = data.CustomerID,
+                    CompanyName = data.CompanyName,
+                    ContactName = data.ContactName,
+                    ContactTitle = data.ContactTitle,
+                    Address = data.Address,
+                    City = data.City,
+                    Region = data.Region,
+                    PostalCode = data.PostalCode,
+                    Country = data.Country,
+                    Phone = data.Phone,
+                    Fax = data.Fax
+                }
+                );
+            }
+        }
     }
 }
+
